d639a225a529aabee08c1424d8184c6e5460aae1
kingokjmh <73475086+kingokjmh@users.noreply.github.com>
2020-11-12 10:55:15 +0800

    Add files via upload

---
+++ Week2_Professional Portfolio Layout/assets/js/main.js
@@ -0,0 +1,8 @@
+console.log("JS is connected!")
+
+/*mySize = ()=>{
+    document.getElementById("widthheight").innerHTML = ('W:' + window.innerWidth+ '____H:' + window.innerHeight)
+}*/
+
+// window.onload = mySize;
+// window.onresize = mySize;
