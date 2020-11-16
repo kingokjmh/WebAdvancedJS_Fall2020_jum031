d6a189ad76abb03d4398c6612348b2513b02ac74
kingokjmh <73475086+kingokjmh@users.noreply.github.com>
2020-11-17 00:00:58 +0800

    Add files via upload

---
+++ Week3_Portfolio_JSON/assets/js/main.js
@@ -0,0 +1,27 @@
+$(()=>{
+    loadData();
+})
+
+
+loadData = () =>{
+    $.getJSON("data.json", (data)=> {
+
+        console.log("JSON is loaded!");
+
+        console.log(data);
+
+        getWebsites(data);
+    })
+
+}
+
+ getWebsites = (temp) =>{
+ let source = $("#my-template").html();
+
+ let template = Handlebars.compile(source);
+
+ let results = template(temp);
+
+  $(".websites-list").append(results);
+
+ }
